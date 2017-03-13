4035556568e625dec0fcda97e8b9c6266e5c15a7
Carlos Doki <carlosdoki@Doki-Econocom-MacBookPro.local>
2017-03-13 11:08:09 -0300

    Final

---
+++ pokedex3/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex3
+//
+//  Created by Carlos Doki on 3/12/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
